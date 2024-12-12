80bfae121b1087180b35b93c98bca1368050fac3
Tre <tregallowayofficial@gmail.com>
2024-12-11 19:47:59 -0600

    just fooling around with styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 
   </head>
-  <body>
+  <body class="bg-gray-100">
       <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <?php echo $slot; ?>
       </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <div class="bg-white">
+    <div class="">
         <div class="container mx-auto px-4 py-10">
           <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
             <!-- Left Column - Single Card -->
@@ -12,25 +12,25 @@
                       <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg>
                   </span>
-                  <h2 class="text-xl font-semibold mb-4">Sup I'm Tre 👋🏾</h2>
-                  <p>Turning problems into solutions with code. While reading a little too much fantasy.</p>
-                <h3>Socials</h3>
+                  <h1 class="text-xl font-semibold mb-4">Sup I'm Tre 👋🏾</h1>
+                  <p class="text-lg">Turning problems into solutions with code. While reading a little too much fantasy.</p>
+                  <div class="mt-6 flex">
+                      <x-fab-youtube class="w-8 h-8 text-gray-600 hover:text-emerald-600 mx-2" />
+                      <x-fab-bluesky class="w-8 h-8 text-gray-600 hover:text-emerald-600 mx-2" />
+                      <x-fab-github class="w-8 h-8 text-gray-600 hover:text-emerald-600 mx-2"/>
 
+                  </div>
                 </div>
                 <div class="px-4 py-4 sm:px-6">
                   <!-- Left card footer -->
-
-                      <p class="max-w-2xl text-balance text-xl font-semibold tracking-tight text-gray-900 sm:text-xl">Want product news and updates? Sign up for our newsletter.</p>
-                      <form class="mt-10 max-w-md">
-                        <div class="flex gap-x-4">
-                          <label for="email-address" class="sr-only">Email address</label>
-                          <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" placeholder="Enter your email">
-                          <button type="submit" class="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Subscribe</button>
-                        </div>
-                      </form>
-
-
-
+                  <p class="max-w-2xl text-balance text-xl font-semibold tracking-tight text-gray-900 sm:text-xl">Want product news and updates? Sign up for our newsletter.</p>
+                  <form class="mt-10 max-w-md">
+                    <div class="flex gap-x-4">
+                      <label for="email-address" class="sr-only">Email address</label>
+                      <input id="email-address" name="email" type="email" autocomplete="email" required class="min-w-0 flex-auto rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" placeholder="Enter your email">
+                      <button type="submit" class="flex-none rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Subscribe</button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
@@ -38,85 +38,70 @@
             <!-- Right Column - Three Cards Stack -->
             <div class="space-y-6">
               <!-- First Card -->
-              <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+              <div class="overflow-hidden rounded-lg bg-white shadow">
                 <div class="px-4 py-5 sm:p-6">
                   <!-- First card main content -->
                   <h2 class="text-xl font-semibold mb-4">Work</h2>
-                    <div class="mx-auto max-w-2xl">
+                  <div class="mx-auto max-w-2xl">
                     <div>
-                                  <p class="text-sm text-gray-500">
-                                    <time datetime="2020-03-16">Mar 16, 2020</time>
-                                  </p>
-                                  <a href="#" class="mt-2 block">
-                                    <p class="text-xl font-semibold text-gray-900">Boost your conversion rate</p>
-                                    <p class="mt-3 text-base text-gray-500">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-                                  </a>
-                                  <div class="mt-3">
-                                    <a href="#" class="text-base font-semibold text-indigo-600 hover:text-indigo-500">Read full story</a>
-                                  </div>
-                                </div>
-                                <div>
-                                        <p class="text-sm text-gray-500">
-                                          <time datetime="2020-03-16">Mar 16, 2020</time>
-                                        </p>
-                                        <a href="#" class="mt-2 block">
-                                          <p class="text-xl font-semibold text-gray-900">Boost your conversion rate</p>
-                                          <p class="mt-3 text-base text-gray-500">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-                                        </a>
-                                        <div class="mt-3">
-                                          <a href="#" class="text-base font-semibold text-indigo-600 hover:text-indigo-500">Read full story</a>
-                                        </div>
-                                      </div>
-                                      <div>
-                                              <p class="text-sm text-gray-500">
-                                                <time datetime="2020-03-16">Mar 16, 2020</time>
-                                              </p>
-                                              <a href="#" class="mt-2 block">
-                                                <p class="text-xl font-semibold text-gray-900">Boost your conversion rate</p>
-                                                <p class="mt-3 text-base text-gray-500">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
-                                              </a>
-                                              <div class="mt-3">
-                                                <a href="#" class="text-base font-semibold text-indigo-600 hover:text-indigo-500">Read full story</a>
-                                              </div>
-                                            </div>
+                      <p class="text-sm text-gray-500">
+                        <time datetime="2020-03-16">Mar 16, 2020</time>
+                      </p>
+                      <a href="#" class="mt-2 block">
+                        <p class="text-xl font-semibold text-gray-900">Boost your conversion rate</p>
+                        <p class="mt-3 text-base text-gray-500">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                      </a>
+                      <div class="mt-3">
+                        <a href="#" class="text-base font-semibold text-indigo-600 hover:text-indigo-500">Read full story</a>
+                      </div>
+                    </div>
+                    <div>
+                      <p class="text-sm text-gray-500">
+                        <time datetime="2020-03-16">Mar 16, 2020</time>
+                      </p>
+                      <a href="#" class="mt-2 block">
+                        <p class="text-xl font-semibold text-gray-900">Boost your conversion rate</p>
+                        <p class="mt-3 text-base text-gray-500">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                      </a>
+                      <div class="mt-3">
+                        <a href="#" class="text-base font-semibold text-indigo-600 hover:text-indigo-500">Read full story</a>
+                      </div>
+                    </div>
+                    <div>
+                      <p class="text-sm text-gray-500">
+                        <time datetime="2020-03-16">Mar 16, 2020</time>
+                      </p>
+                      <a href="#" class="mt-2 block">
+                        <p class="text-xl font-semibold text-gray-900">Boost your conversion rate</p>
+                        <p class="mt-3 text-base text-gray-500">Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.</p>
+                      </a>
+                      <div class="mt-3">
+                        <a href="#" class="text-base font-semibold text-indigo-600 hover:text-indigo-500">Read full story</a>
+                      </div>
+                    </div>
                   </div>
-
-
-                </div>
-                <div class="px-4 py-4 sm:px-6">
-                  <!-- First card footer -->
-                  <p>Footer content</p>
                 </div>
               </div>
 
               <!-- Second Card -->
-              <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+              <div class="overflow-hidden rounded-lg bg-white shadow">
                 <div class="px-4 py-5 sm:p-6">
                   <!-- Second card main content -->
                   <h2 class="text-xl font-semibold mb-4">Right Column Card 2</h2>
                   <p>Content for second card</p>
                 </div>
-                <div class="px-4 py-4 sm:px-6">
-                  <!-- Second card footer -->
-                  <p>Footer content</p>
-                </div>
               </div>
 
               <!-- Third Card -->
-              <div class="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
+              <div class="overflow-hidden rounded-lg bg-white shadow">
                 <div class="px-4 py-5 sm:p-6">
                   <!-- Third card main content -->
                   <h2 class="text-xl font-semibold mb-4">Right Column Card 3</h2>
                   <p>Content for third card</p>
                 </div>
-                <div class="px-4 py-4 sm:px-6">
-                  <!-- Third card footer -->
-                  <p>Footer content</p>
-                </div>
               </div>
             </div>
           </div>
         </div>
-
     </div>
 </x-layout>
