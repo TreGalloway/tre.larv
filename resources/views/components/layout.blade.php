45e9efee691dc58f7b60ecc474975de2acfad8b0
Tre <tregallowayofficial@gmail.com>
2025-01-16 17:07:31 -0600

    getting better idea of site styling

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Title</title>
+    <title>Tregalloway.com</title>
     <script src="https://cdn.tailwindcss.com"></script>
 
   </head>
-  <body class="bg-gray-100">
-      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+  <body class="bg-gray-150">
+      <div class="mx-auto max-w-7xl px-4 mt-6 sm:px-6 lg:px-8">
           <?php echo $slot; ?>
       </div>
 
